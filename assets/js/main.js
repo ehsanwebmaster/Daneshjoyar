c4fdbdd80e39cf226024911ab371e1c9a30b9ccf
Ali Farahmand <74143303+farahmandaliir@users.noreply.github.com>
2022-12-28 21:14:02 +0330

    add mobile slider

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$("#blogslider").owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 30,
+    nav: false,
+    center: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+      1200: {
+        items: 1,
+      },
+    },
+  });
