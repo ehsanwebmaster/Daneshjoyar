169980cc84aa9c639aa06ddcf8fb865acf940560
Ehsan Farahmand <89188159+ehsanwebmaster@users.noreply.github.com>
2023-01-29 14:19:24 +0330

    index page completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,28 @@ $("#indexslide2").owlCarousel({
     },
   },
 });
+$("#indexslide3").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 15,
+  nav: false,
+  center: true,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
